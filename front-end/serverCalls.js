2c35b5f07b4594cec095128259e6f5c17b43c0f7
cristian3629 <cristian.gonzalez@despegar.com>
2017-08-20 20:59:26 -0300

    elimino los archivos que conectaban con la API de google y dejo en el esquema para conectarse al server

---
+++ front-end/serverCalls.js
@@ -0,0 +1,18 @@
+// Load the API and make an API call.  Display the results on the screen.
+
+
+//En esta funcion obtengo el titulo del video que me interesa.
+function getTitle(){
+
+}
+
+
+
+//En esta funcion obtengo el id del caption que me interesa
+function getIdCaption(){
+}
+
+//En esta funcion dado que ya obtuve el id del caption, ahora descargo el
+//caption
+function getCaption(idCaption){
+}
