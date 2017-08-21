d0026b6f416bc9d10a9bcb363f192abc0654a497
cristian3629 <cristian.gonzalez@despegar.com>
2017-08-20 21:41:06 -0300

    preparo el codigo para poder realizar el request al server# "

--- front-end/serverCalls.js
+++ front-end/serverCalls.js
@@ -1,18 +1,10 @@
-// Load the API and make an API call.  Display the results on the screen.
+var ip;
+var port;
 
-
-//En esta funcion obtengo el titulo del video que me interesa.
-function getTitle(){
-
-}
-
-
-
-//En esta funcion obtengo el id del caption que me interesa
-function getIdCaption(){
-}
-
-//En esta funcion dado que ya obtuve el id del caption, ahora descargo el
-//caption
-function getCaption(idCaption){
+function serverCall() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", 'http://'+ip+':'+port+'/'+videoId, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
 }
